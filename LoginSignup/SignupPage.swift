4a881cf85864939238d27351d981bcbe3a46b3e3
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-06-04 10:43:47 +0300

    Sign up page

--- LoginSignup/SignupPage.swift
+++ LoginSignup/SignupPage.swift
@@ -9,8 +9,75 @@
 import SwiftUI
 
 struct SignupPage: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Text("Sign up")
+                .font(.largeTitle)
+                .bold()
+                .padding()
+            Text("Create an account, It's free")
+                .foregroundColor(.secondary)
+                .padding()
+            
+            Group {
+                VStack {
+                    HStack {
+                        Text("Email")
+                            .foregroundColor(.secondary)
+                        Spacer()
+                    }
+                    TextField("", text: $email)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
+                VStack {
+                    HStack {
+                        Text("Password")
+                            .foregroundColor(.secondary)
+                        Spacer()
+                    }
+                    SecureField("", text: $email)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
+                VStack {
+                    HStack {
+                        Text("Confirm Password")
+                            .foregroundColor(.secondary)
+                        Spacer()
+                    }
+                    SecureField("", text: $confirmPassword)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
+            }.padding()
+            
+            Spacer()
+            
+            Button(action: {
+                
+            }) {
+                Text("Sign up")
+                    .foregroundColor(.black)
+                    .padding()
+                    .frame(width: 300)
+                    .background(Color("green")
+                        .cornerRadius(20)
+                        .offset(y: 2))
+                    .overlay(RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color.black, lineWidth: 1))
+            }.padding()
+            
+            Spacer()
+            
+            HStack {
+                Text("Already have an account?")
+                Text("Login")
+                    .bold()
+            }
+        }
     }
 }
 
